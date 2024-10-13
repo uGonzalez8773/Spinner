a05efdb251897bd7dbffc8c414ec3f90c774be1d
caarlosdamian <dapecarlos@gmail.com>
2024-10-13 09:53:58 -0600

    fix

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
-import { SvgSpinnersPulseRingsMultiple } from "./SvgSpinnersPulseRingsMultiple";
+import { useState } from 'react';
+import { SvgSpinnersPulseRingsMultiple } from './SvgSpinnersPulseRingsMultiple';
 
 const Spinner = (ComponentToWrap) => {
   const WrappedWithSpinner = (props) => {
     const [loading, setLoading] = useState(true);
 
     const stopLoading = () => {
-        console.log("loading to false");
-        setLoading(false)
-      };
+      setLoading(false);
+    };
 
-    if (loading) {
-      return (
-        <div className="flex flex-col justify-center items-center">
-            <SvgSpinnersPulseRingsMultiple />
-        </div>
-      );
-    }
+    const SpinnerContent = () => (
+      <div className="flex flex-col justify-center items-center">
+        <SvgSpinnersPulseRingsMultiple />
+      </div>
+    );
 
-    return <ComponentToWrap setLoading={stopLoading} {...props} />;
+    return (
+      <ComponentToWrap setLoading={stopLoading} loading={loading} {...props}>
+        <SpinnerContent />
+      </ComponentToWrap>
+    );
   };
 
   WrappedWithSpinner.displayName = `Spinner(${
-    ComponentToWrap.displayName || ComponentToWrap.name || "Component"
+    ComponentToWrap.displayName || ComponentToWrap.name || 'Component'
   })`;
 
   return WrappedWithSpinner;
